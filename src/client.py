6e0bbb5c1487c90acd23163f81e101bdc00f74d7
unknown <vk.brik@gmail.com>
2024-11-27 23:49:06 +0200

    task 2

---
+++ src/client.py
@@ -0,0 +1,30 @@
+import serial
+import time
+
+
+def run_client():
+    try:
+        ser = serial.Serial('COM4', 9600, timeout=1)
+        print("Client is connected on COM4...")
+
+        if ser.is_open:
+            message = "Hello, Virtual Server!"
+            ser.write(f"{message}\n".encode())
+            print(f"Sent to server: {message}")
+
+            time.sleep(1)
+            response = ser.readline().decode().strip()
+            print(f"Received from server: {response}")
+        else:
+            print("Failed to open serial port.")
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+    except KeyboardInterrupt:
+        print("Client shutting down.")
+    finally:
+        ser.close()
+        print("Serial port closed.")
+
+
+if __name__ == "__main__":
+    run_client()
